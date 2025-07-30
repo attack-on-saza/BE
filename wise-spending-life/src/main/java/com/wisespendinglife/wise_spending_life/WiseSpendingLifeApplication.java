b7ebef80f6eb3304010442cb718e3bf677ea4b11
holychan <east0321@outlook.kr>
2025-07-30 18:49:22 +0900

    Init: 프로젝트 초기화 및 DB 세팅

---
+++ wise-spending-life/src/main/java/com/wisespendinglife/wise_spending_life/WiseSpendingLifeApplication.java
@@ -0,0 +1,13 @@
+package com.wisespendinglife.wise_spending_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseSpendingLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseSpendingLifeApplication.class, args);
+	}
+
+}
---
+++ wise-spending-life/src/test/java/com/wisespendinglife/wise_spending_life/WiseSpendingLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisespendinglife.wise_spending_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiseSpendingLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
